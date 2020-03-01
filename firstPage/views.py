aa98856bd6011ac882e73b7b8b7a5ea8794dcd9d
vikashvishnu1508 <vikashvishnu1508@gmail.com>
2020-03-02 00:47:56 +0530

    created signup page edited profile details

--- firstPage/views.py
+++ firstPage/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import Department, Role, UserDetails
+from .models import Department, Role
+from .forms import SignUpForm
 # Create your views here.
 def index(request):
     # context = {
@@ -32,3 +34,23 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'firstPage/logout.html', {'message': 'Logout sucessfuly'})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.profile.address = form.cleaned_data.get('address')
+            user.profile.phoneNumber = form.cleaned_data.get('phoneNumber')
+            user.profile.role = form.cleaned_data.get('role')
+            user.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        form = SignUpForm()
+    return render(request, 'firstPage/signup.html', {'form': form})
