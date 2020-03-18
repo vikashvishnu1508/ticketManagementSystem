42123aa47417fbd12ecdafcbe45499e687e3d68c
vikashvishnu1508 <vikashvishnu1508@gmail.com>
2020-03-18 09:16:45 +0530

    signup page level2

--- firstPage/forms.py
+++ firstPage/forms.py
@@ -5,35 +5,57 @@ from django.forms import ModelChoiceField
 
 from .models import Role
 
-# test = Role
-# test1 = Role.objects.all()
-
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30, required=False, help_text='Required.',
-                                 widget=forms.TextInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(max_length=30, required=False, help_text='Required.',
-                                 widget=forms.TextInput(attrs={'class':'form-control'}))
+                                 widget=forms.TextInput(attrs={'class':'form-control col-md-6 float-left',
+                                                               'placeholder':'First Name *'}))
     last_name = forms.CharField(max_length=30, required=False, help_text='Required.',
-                                 widget=forms.TextInput(attrs={'class':'form-control'}))
+                                 widget=forms.TextInput(attrs={'class':'form-control col-md-6 float-right',
+                                                               'placeholder':'Last Name *'}))
+    
+    username = forms.CharField(max_length=30, required=False, help_text='Required.',
+                                 widget=forms.TextInput(attrs={'class':'form-control col-md-6 float-left',
+                                                               'placeholder':'Username *'}))
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.',
-                                 widget=forms.EmailInput(attrs={'class':'form-control'}))
+                                 widget=forms.EmailInput(attrs={'class':'form-control col-md-6 float-right',
+                                                               'placeholder':'Email Address *'}))
+    
     password1 = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.',
-                                 widget=forms.PasswordInput(attrs={'class':'form-control'}))
+                                 widget=forms.PasswordInput(attrs={'class':'form-control col-md-6 float-left',
+                                                               'placeholder':'Password *'}))
     password2 = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.',
-                                 widget=forms.PasswordInput(attrs={'class':'form-control'}))
+                                 widget=forms.PasswordInput(attrs={'class':'form-control col-md-6 float-right',
+                                                               'placeholder':'Re-enter password *'}))
+    
     birth_date = forms.DateField(help_text="Required. Format: YYYY-MM-DD",
-                                 widget=forms.DateInput(attrs={'class':'form-control'}))
-    address = forms.CharField(max_length=8000, help_text='Required.',
-                                 widget=forms.Textarea(attrs={'class':'form-control'}))
+                                 widget=forms.DateInput(attrs={'class':'form-control col-md-6 float-left',
+                                                               'placeholder':'Please enter Birth Date *'}))
     phoneNumber = forms.CharField(max_length=10, help_text='Required.',
-                                 widget=forms.TextInput(attrs={'class':'form-control'}))
+                                 widget=forms.TextInput(attrs={'class':'form-control col-md-6 float-right',
+                                                               'placeholder':'Phone Number *'}))
+    
+    address = forms.CharField(max_length=8000, help_text='Required.',
+                                 widget=forms.Textarea(attrs={'class':'form-control',
+                                                               'placeholder':'Address *',
+                                                               'style': 'height: 5em;'}))
+    
     role = forms.ModelChoiceField(Role.objects.all(),
-                            widget=forms.Select(attrs={'class':'ddl form-control'}),
+                            widget=forms.Select(attrs={'class':'ddl form-control',
+                                                        'placeholder':'Please select a role *'}),
                             to_field_name="name",
                             empty_label=None)
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'birth_date', 'address', 'phoneNumber', 'role',)
+        fields = ('first_name',
+                  'last_name',
+                  'username',
+                  'email',
+                  'password1',
+                  'password2',
+                  'birth_date',
+                  'phoneNumber',
+                  'address',
+                  'role',)
 
     # def __init__(self, *args, **kwargs):
     #     super(Role, self).__init__(*args, **kwargs)
