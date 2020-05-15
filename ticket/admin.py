424a4d4b1056bd2a14ba29d72a880d72401f0e89
vikashvishnu1508 <vikashvishnu1508@gmail.com>
2020-05-15 21:11:06 +0530

    remove pycache

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from ticket.models import Product, IssueType, Priority, Status, Issue
+from ticket.models import Product, IssueType, Priority, Status, Issue, IssueUpdateDetails, IssueAssignmentDetails, InvestigationDetails
 
 # Register your models here.
 admin.site.register(Product)
@@ -8,3 +8,6 @@ admin.site.register(IssueType)
 admin.site.register(Priority)
 admin.site.register(Status)
 admin.site.register(Issue)
+admin.site.register(IssueUpdateDetails)
+admin.site.register(IssueAssignmentDetails)
+admin.site.register(InvestigationDetails)
